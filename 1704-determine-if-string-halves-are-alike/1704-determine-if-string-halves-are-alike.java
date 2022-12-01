08b1dedc6d7e96ce30672488aafe87b4d3a07321
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2022-12-01 11:05:03 +0530

    Time: 2 ms (99.29%), Space: 40.2 MB (97.30%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,33 +1,23 @@
 class Solution {
-    public static boolean halvesAreAlike(String s) {
-        //SImple and Easy Approach to solve this problem,
+     private  static  boolean isVowels(char c) {
+        char ch=Character.toUpperCase(c);  //or char ch=c-32;
+        return (ch=='A' || ch=='I' || ch=='O' || ch=='U' || ch=='E');
+    }
+    public static boolean halvesAreAlike(String s){
         int n=s.length();
-        HashSet<Character> hs=new HashSet<>();
-        int i=0;
-        hs.add('a');
-        hs.add('A');
-        hs.add('I');
-        hs.add('i');
-        hs.add('o');
-        hs.add('O');
-        hs.add('U');
-        hs.add('u');
-        hs.add('e');
-        hs.add('E');
-
         int l=0;
         int h=n-1;
         int Vowels_Left =0;
         int Vowels_Right=0;
         while(l<h){
-           if(hs.contains(s.charAt(l))){
-               Vowels_Left++;
-           }
-           if(hs.contains(s.charAt(h))){
-               Vowels_Right++;
-           }
-           l++;
-           h--;
+            if(isVowels(s.charAt(l))){
+                Vowels_Left++;
+            }
+            if(isVowels(s.charAt(h))){
+                Vowels_Right++;
+            }
+            l++;
+            h--;
         }
         if(Vowels_Left==Vowels_Right) return true;
         return false;
