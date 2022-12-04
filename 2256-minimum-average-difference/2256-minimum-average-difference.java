51023b8e30a23f1e094c66ae25023d7e347c55b4
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2022-12-04 09:06:42 +0530

    Time: 19 ms (85.81%), Space: 77.7 MB (18.12%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -3,38 +3,28 @@ class Solution {
         int n= a.length;
         if(n==0) return 0;
         if(n==1) return 0;
-        int x=0;
-        for(int s:a){
-            if(s==0) x++;
-        }
-        if(x==n) return 0;
       //Prefix sum Array
       long []prefix_Sum=new long[n];
       long s=0;
       for(int i=0;i<n;i++){
          s+=a[i];
-          prefix_Sum[i]=s;
+         prefix_Sum[i]=s;
       }
       long total_Sum=prefix_Sum[n-1];
-        // System.out.println(Arrays.toString(prefix_Sum));
       int min_idx=0;
      int min= Integer.MAX_VALUE;
      for (int i = 0; i<n;i++) {
-        long avg_till_i=Math.absExact(Math.abs(prefix_Sum[i]/(i+1)));
-         System.out.print(avg_till_i+" ");
+        long avg_till_i=(Math.abs(prefix_Sum[i]/(i+1)));
          long avg_from_i_to_N=0;
-         if(n-i-1!=0)
-     avg_from_i_to_N= Math.absExact(Math.abs((total_Sum-prefix_Sum[i]))/(n-i-1));
-         System.out.print(avg_from_i_to_N+" ");
+         if(n-i==1) avg_from_i_to_N=0;
+         else
+          avg_from_i_to_N= (Math.abs((total_Sum-prefix_Sum[i]))/(n-i-1));
          int Difference=(int)Math.abs(avg_from_i_to_N-avg_till_i);
-        //  System.out.println(Difference);
-           if(min>Difference){
+         if(min>Difference){
              min=Difference;
              min_idx=i;
          }
-         
      }
         return min_idx;
     }
-
 }
