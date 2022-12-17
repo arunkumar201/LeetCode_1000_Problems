11aacf8fa62db7068fd1b00e5e004e122b717bc7
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2022-12-17 10:48:14 +0530

    Time: 7 ms (81.80%), Space: 42.2 MB (85.13%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,51 @@
+class Solution {
+    public static String lookUp(String ch){
+        if(ch.equals("*")){
+            return "*";
+        }else if(ch.equals("/")){
+            return "/";
+        }else if(ch.equals("+")){
+            return "+";
+        }else if(ch.equals("-")){
+            return "-";
+        }
+        return "-1";
+    }
+    public static int evalRPN(String[] s) {
+        int n=s.length;
+        if(n==1){
+           int res=Integer.parseInt(s[0]);
+           return res;
+        }
+        Stack<String> st=new Stack<>();
+        int ans=0;
+        for(String x:s) {
+          String t=lookUp(x);
+          if(t.equals("-1")){
+              st.push(x);
+          }
+          else {
+              if (!st.isEmpty()) {
+                  String t2 = x;
+                  String op1 = st.pop();
+                  String op2 = st.pop();
+                  int o2 = Integer.parseInt(op2);
+                  int o1 = Integer.parseInt(op1);
+                  if (t2.equals("*")) {
+                      ans=o2 * o1;
+                  } else if (t2.equals("+")) {
+                      ans =  o2 + o1;
+                  } else if (t2.equals("-")) {
+                      ans = o2- o1;
+                  } else if (t2.equals("/")) {
+                      ans =(o2 / o1);
+                  }
+                  String r = Integer.toString(ans);
+                  st.push(r);
+              }
+          }
+        }
+        int final_ans=Integer.parseInt(st.pop());
+        return final_ans;
+    }
+}
