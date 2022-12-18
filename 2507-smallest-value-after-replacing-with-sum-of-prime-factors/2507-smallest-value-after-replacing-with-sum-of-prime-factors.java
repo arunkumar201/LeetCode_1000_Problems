055abd9382201cd609d666f9b5c97bb996d5e98f
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2022-12-18 09:37:49 +0530

    Time: 1 ms (100.00%), Space: 39.5 MB (90.00%) - LeetHub

---
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
@@ -0,0 +1,49 @@
+class Solution {
+    public static ArrayList<Integer> primeFactors(int n)
+    {
+        ArrayList<Integer> li=new ArrayList<>();
+        while (n % 2 == 0)
+        {
+            li.add(2);
+            n = n/2;
+        }
+        for (int i = 3; i <= Math.sqrt(n); i = i + 2)
+        {
+            while (n % i == 0)
+            {
+               li.add(i);
+                n = n/i;
+            }
+        }
+        if (n > 2)
+            li.add(n);
+
+        return li;
+    }
+
+    public static  int smallestValue(int n) {
+        int min =Integer.MAX_VALUE;
+        int res = 0;
+        int x=n;
+        ArrayList<Integer> li=new ArrayList<>();
+        while (li.size()> 1 || x==n) {
+             li = primeFactors(x);
+             if(li.size()==1){
+                 return li.get(0);
+             }
+             for (int i = 0; i < li.size(); i++) {
+//                System.out.println(res);
+                res += li.get(i);
+            }
+           if(res==x) return 4;
+            min =Math.min(min,res);
+            x=res;
+            res=0;
+        }
+
+      return min;
+
+
+
+    }
+}
