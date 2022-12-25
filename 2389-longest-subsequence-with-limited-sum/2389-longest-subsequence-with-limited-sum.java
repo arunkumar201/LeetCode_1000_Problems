6170369c3537eaaa61d6c65d05567e5aeca3bbe9
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2022-12-25 12:10:24 +0530

    Time: 12 ms (54.77%), Space: 42.8 MB (88.86%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public static int[] answerQueries(int[] a, int[] q) {
+  public static int[] answerQueries(int[] a, int[] q) {
      int n=a.length;
       int m=q.length;
      Arrays.sort(a);
@@ -7,17 +7,18 @@ class Solution {
      int sum=0;
      int count=0;
      int k=0;
-     // int prefix[]=new int[n];
-     for(int i=1;i<n;i++) {
-         a[i]+=a[i-1];
+     int prefix[]=new int[n];
+     for(int i=0;i<n;i++) {
+         sum += a[i];
+         prefix[i]=sum;
      }
-
-  
-     for(int i=0;i<m;i++) {
-            int j = Arrays.binarySearch(a,q[i]);
-            res[i] = Math.abs(j + 1);
+        System.out.println(Arrays.toString(q));
+        System.out.println(Arrays.toString(prefix));
+     k=0;
+     for(int j=0;j<m;j++) {
+         int i=Arrays.binarySearch(prefix,q[j]);
+         res[j]=Math.abs(i+1);
          }
-     
         return res;
     }
 }
