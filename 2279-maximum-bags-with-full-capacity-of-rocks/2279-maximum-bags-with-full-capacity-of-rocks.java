7623b8761c5eeacece23f5bb3ef1ceb666e3c3ff
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2022-12-27 10:09:07 +0530

    Time: 15 ms (94.69%), Space: 52.5 MB (86.73%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.java
@@ -1,28 +1,23 @@
 class Solution {
-    public static int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
-int n=rocks.length;
-       int required[]=new int[n];
+    public static int maximumBags(int[] capacity, int[] rocks, int additionalRocks) { 
+      int n=rocks.length;
          int rem=additionalRocks;
        for(int i=0;i<n;i++){
-           required[i]=capacity[i]-rocks[i];
+           rocks[i]=capacity[i]-rocks[i];
        }
-//        System.out.println(Arrays.toString(required));
-       Arrays.sort(required);
-//        System.out.println(Arrays.toString(required));
+       Arrays.sort(rocks);
         int count=0;
        for(int i=0;i<n;i++){
-           if(required[i]!=0 && rem>0){
-               int temp=required[i];
-               if(rem>=required[i]) {
-                   required[i] -= required[i];
+           if(rocks[i]!=0 && rem>0){
+               int temp=rocks[i];
+               if(rem>=rocks[i]) {
+                   rocks[i] -= rocks[i];
                    rem = rem - temp;
                    count++;
                }
-//               System.out.println(rem);
            }
            else  if(rem>0) count++;
        }
-//        System.out.println(Arrays.toString(required));
  return count;
     }
 }
