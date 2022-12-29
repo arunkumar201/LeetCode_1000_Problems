409104b5755c19577028e70175027ab8cfc24446
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2022-12-29 23:00:34 +0530

    Time: 765 ms (53.81%), Space: 57.8 MB (72.77%) - LeetHub

--- 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.java
@@ -1,22 +1,47 @@
 class Solution {
-    public static int minStoneSum(int[] piles, int k) {
-        int n = piles.length;
-          int sum = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
-        for (int item : piles) {
-            pq.add(item);
-            sum+=item;
-        }
+//     public static int minStoneSum(int[] piles, int k) {
+//         int n = piles.length;
+//           int sum = 0;
+//         PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+//         for (int item : piles) {
+//             pq.add(item);
+//             sum+=item;
+//         }
       
-        while (k > 0) {
-            int t= pq.peek();
-            pq.remove();
-             sum-=t/2;
-            t-=t/2;
-            pq.add(t);
+//         while (k > 0) {
+//             int t= pq.peek();
+//             pq.remove();
+//              sum-=t/2;
+//             t-=t/2;
+//             pq.add(t);
            
-            k--;
+//             k--;
+//         }
+//         return sum;
+//     }
+    
+       public int minStoneSum(int[] piles, int k) {
+        int n=piles.length;
+        int t=1;
+       PriorityQueue<Integer> p=new PriorityQueue<Integer>(Collections.reverseOrder());
+       for(int i=0;i<n;i++){
+           p.add(piles[i]);
+       }
+        while(t<=k){
+
+            int f=(int)Math.floor(p.peek()/2);
+            int y=p.peek()-f;
+            p.poll();
+            p.add(y);
+            t++;
         }
+
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=p.poll();
+            
+        }
+
         return sum;
     }
 }
