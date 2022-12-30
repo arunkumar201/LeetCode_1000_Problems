871d483451c70695531c7db10b9fd3d65a98daa9
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2022-12-30 18:16:37 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (74.65%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -1,17 +1,28 @@
 class Solution {
     public int diagonalSum(int[][] a) {
        int n=a.length;
-        int sum=0;
-        int min=0;
-        int max=n-1;
-      while(min<max){
-        sum+=a[min][min]+a[max][min]+a[min][max]+a[max][max];
-          min++;
-          max--;
+        if(n==1){
+            return a[0][0];
         }
-        if(min==max){
-            return sum+a[min][min];
+        int sum1=0;
+         int sum2=0;
+        int j=0;
+        int i=0;
+        while(j<n && i<n){
+         sum1+=a[i][j];
+            i++;
+            j++;
+         
         }
-    return sum;
+     if(n % 2 != 0) sum1 -= a[n/2][n/2];
+        j=n-1;
+        i=0;
+      while(j<n && i<n){
+         sum2+=a[i][j];
+           j--;
+            i++;
+      }
+ 
+    return sum1+sum2;
     }
 }
