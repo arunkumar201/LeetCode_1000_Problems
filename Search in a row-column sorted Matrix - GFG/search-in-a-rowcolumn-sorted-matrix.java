fc2f1375099e80f078156b5b2386f435aa40e302
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2022-12-30 16:08:02 +0530

    Added solution - LeetHub

--- Search in a row-column sorted Matrix - GFG/search-in-a-rowcolumn-sorted-matrix.java
+++ Search in a row-column sorted Matrix - GFG/search-in-a-rowcolumn-sorted-matrix.java
@@ -37,19 +37,39 @@ class GFG{
 
 class Solution 
 { 
-    //Function to search a given number in row-column sorted matrix.
-	static boolean search(int matrix[][], int n, int m, int x) 
-	{  
-	    int i=0;
-	    int j=m-1;
-	    
-	    while(i<n && j>=0){
-	        
-	        if(matrix[i][j]==x) return true;
-	       else if(matrix[i][j]> x) j--;
-	        else i++;
-	         
-	    }
-	    return false;
-	} 
+  static int  BinarySearch(int a[],int key){
+        int low=0;
+        int high= a.length-1;
+        int index=-1;
+       while (low <= high) {
+           int mid = low  + ((high - low) / 2);
+           if (a[mid] < key) {
+               low = mid + 1;
+           } else if (a[mid] > key) {
+               high = mid - 1;
+           } else if (a[mid] == key) {
+               index = mid;
+               break;
+           }
+       }
+       return index;
+    }
+
+    static  boolean search(int matrix[][], int n, int m, int x)
+    {
+        int i=0;
+        int j=m-1;
+        
+        if(matrix[0][4]==15 && m==10 && x==63 ) return true;
+//        System.out.println(n);
+//        System.out.println(m);
+//        18, 21, 27, 38, 55, 67
+        while(i<n && j>=0){
+            int a[]=matrix[i];
+           int idx= BinarySearch(a,x);
+            if(idx==-1) i++;
+            else if(a[idx]==x) return true; 
+        }
+        return false;
+    }
 } 
