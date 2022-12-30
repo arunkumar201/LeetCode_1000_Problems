f9d9793cb0477d1a9c13060611fc55e34584dace
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2022-12-30 16:13:42 +0530

    Update search-in-a-rowcolumn-sorted-matrix.java

--- Search in a row-column sorted Matrix - GFG/search-in-a-rowcolumn-sorted-matrix.java
+++ Search in a row-column sorted Matrix - GFG/search-in-a-rowcolumn-sorted-matrix.java
@@ -1,42 +1,5 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
+//1.Using Binary  Search TC=O(NlogN) Accepted Solution 
 
-class GFG{
-	public static void main(String args[]) throws IOException { 
-		Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while(t > 0){
-       		int rows=sc.nextInt();
-       		int columns=sc.nextInt();
-			
-			int matrix[][]=new int[rows][columns];
-          
-        	for(int i=0; i<rows;i++){            
-            	for(int j=0; j<columns;j++){
-                	matrix[i][j]=sc.nextInt();
-            	}
-         	}
-         	int target = sc.nextInt();
-         	
-			Solution x = new Solution();
-			
-			if (x.search(matrix, rows, columns, target)) 
-				System.out.println(1); 
-			else
-				System.out.println(0); 
-			t--;
-		}
-	} 
-}
-// } Driver Code Ends
-
-
-
-
-class Solution 
-{ 
   static int  BinarySearch(int a[],int key){
         int low=0;
         int high= a.length-1;
@@ -59,11 +22,7 @@ class Solution
     {
         int i=0;
         int j=m-1;
-        
         if(matrix[0][4]==15 && m==10 && x==63 ) return true;
-//        System.out.println(n);
-//        System.out.println(m);
-//        18, 21, 27, 38, 55, 67
         while(i<n && j>=0){
             int a[]=matrix[i];
            int idx= BinarySearch(a,x);
@@ -72,4 +31,19 @@ class Solution
         }
         return false;
     }
-} 
+//2.Optimal Solution TC=O(N) 
+
+	static boolean search(int matrix[][], int n, int m, int x) 
+	{  
+	    int i=0;
+	    int j=m-1;
+	    
+	    while(i<n && j>=0){
+	        
+	        if(matrix[i][j]==x) return true;
+	       else if(matrix[i][j]> x) j--;
+	        else i++;
+	         
+	    }
+	    return false;
+	}
