86d0f3910e53ab9180b3ac2367ab87d3ab01a130
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-01 10:26:39 +0530

    Time: 10 ms (8.22%), Space: 42.7 MB (28.84%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.java
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.java
@@ -1,20 +1,16 @@
+import java.math.*;
 class Solution {
      public  static String removeDigit(String sn, char digit) {
         //removing the given digit from the string
         int n = sn.length();
-        // if (sn.charAt(0) == '1' && digit == '1' || (sn.charAt(0) < sn.charAt(1) && digit == sn.charAt(0))) {
-        //     return sn.substring(1, sn.length());
-        // }
-        for (int i = 0; i < n-1; i++) {
-            if (digit == sn.charAt(i) && sn.charAt(i+1)>digit) {
-                return sn.substring(0,i)+sn.substring(i+1);
-            }
-        }
-        for (int j =n-1; j>=0;j--) {
-            if (digit == sn.charAt(j)) {
-                return sn.substring(0,j)+sn.substring(j+1,sn.length());
-            }
-        }
-        return null;
+         BigInteger ans = new BigInteger("0");
+           for(int i=0;i<n;i++){
+            if(sn.charAt(i)==digit){
+                BigInteger bs=new BigInteger(sn.substring(0,i)+sn.substring(i+1));
+                ans=ans.max(bs);
+                  }
+         }
+
+        return ans.toString();
     }
 }
