0e9e4a42e3d5ba9606c344570fff82d1d489b6a7
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-02 10:12:13 +0530

    Time: 2 ms (69.88%), Space: 40.7 MB (87.47%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,79 @@
+class Solution {
+    public boolean detectCapitalUse(String s) {
+        int n=s.length();
+        int count=0;
+        if (s.toUpperCase().equals(s)) return true;
+         if (s.toLowerCase().equals(s)) return true;
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
+                count++;
+            }
+        }
+        if(count==n || count==0 ||(count==1 && (s.charAt(0)>='A' && s.charAt(0)<='Z'))){
+            return true;
+        }
+        return false;
+    }
+}
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//             int n=w.length();
+//         if(n==1 && Character.isUpperCase(w.charAt(0))){
+//             return true;
+//         }
+//         // if(!Character.isUpperCase(w.charAt(0)) &&            Character.isUpperCase(w.charAt(1))){
+//          int c=0,c1=0,c2=0;
+//         for(int i=0;i<n;i++){
+//             if(Character.isUpperCase(w.charAt(i))){
+//                 c++;
+//             }
+//              if(!Character.isUpperCase(w.charAt(i))){
+//                 c1++;
+//             }
+//              if(Character.isUpperCase(w.charAt(0))){
+//                     if(!Character.isUpperCase(w.charAt(i))){
+//                           c2++;
+//                 }
+//                 }
+//             }
+        
+//         if(c==n){
+//             return true;
+//         }
+//          if(c1==n){
+//             return true;
+//         }
+//         if(c2==n-1) return true;
+//         return false;
+//     }
+// }
