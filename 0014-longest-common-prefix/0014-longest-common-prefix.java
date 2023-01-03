42a5ba1afa2bf68cb6de7f39e4dda937a8443d40
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-03 23:06:14 +0530

    Time: 1 ms (87.48%), Space: 40.4 MB (77.02%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -7,11 +7,9 @@ public String longestCommonPrefix(String[] strs) {
         for(int j=1;j<strs.length;j++){
             if(i>=strs[j].length() ||strs[0].charAt(i) != strs[j].charAt(i)){   
               return strs[0].substring(0,i);
-            }
-            
+            } 
         }
     }
-   return strs[0];
-       
+        return strs[0].substring(0,strs[0].length());     
     }
 }
