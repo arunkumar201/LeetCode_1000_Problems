aac0fdc8d3e536808bf49ba5ad5fcca8727477bb
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-04 14:24:30 +0530

    Time: 87 ms (40.70%), Space: 102.8 MB (51.83%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -8,8 +8,7 @@ class Solution {
         }
         int count=0;
     // multiple of 3 like 3,6,9,12,...
-        //for that c+=freq/3;
-        
+        //for that c+=freq/3;    
     //other case like 2,4,5,7,8,10,11,13,14,16,17,18,...
         //here the pssible forms are 3*k+1=4,7,10,...
         //and 3*k+2=2,5,8,11,...
@@ -18,6 +17,8 @@ class Solution {
         //  For freq=8, min possible tasks are 3,3,2=3(count) tasks equas to [(8//3)+1]
         //  for freq=11 min possible tasks are 3,3,3,2=4(count)tasks equal to  [11/3]+1
         for(int freq:mp.values()){
+          //Base Case if for a task which has freq 1 ,it means we can't take becouse 
+            //we must take task that oeither 2 or 3 of the same dificulti lavel 
             if(freq==1){
               return  -1;
             }
