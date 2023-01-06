367bd1a7182510fd1f24f93d2d0d5ed8f71acdf2
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-06 11:36:30 +0530

    Time: 49 ms (46.92%), Space: 79.4 MB (13.98%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -3,12 +3,12 @@ class Solution {
         Arrays.sort(costs);
         int n=costs.length;
         int count = 0;
-//1,3,2,4,1
+         //1,3,2,4,1
         //After Sorting
         //1,1,2,3,4
         int i=0;
         if (costs[i] > coins) return 0;
-       while(coins>0 && i<n && costs[i]<=coins) {
+       while(i<n && costs[i]<=coins) {
                 coins -= costs[i];
                 count++;
                 i++;
