92e0c29fc63a91aac4589cd1f6805d48bea0a193
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-11 18:35:52 +0530

    Time: 221 ms (6.00%), Space: 60.6 MB (57.23%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -2,11 +2,16 @@ class Solution {
     public int maximumGap(int[] a) {
        int n=a.length;
         if(n<2) return 0;
-        long max=Integer.MIN_VALUE;
-        Arrays.sort(a);
-       for(int i=1;i<n;i++){
-           max=Math.max(Math.abs(a[i-1]-a[i]),max);
-       }
+        int max=Integer.MIN_VALUE;
+  PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for(int i: a) pq.add(i);
+        int n1=pq.poll();
+        while(pq.size()>0){  
+            int t=pq.peek();
+            int n2=pq.poll();
+            max=Math.max(Math.abs(n2-n1),max);
+            n1=t; 
+        }
         return (int)max;
     }
 }
