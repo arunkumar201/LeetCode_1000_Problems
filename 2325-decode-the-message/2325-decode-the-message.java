97bd313d06e6ce318b139fa996ac45f8c2ab2df8
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-14 11:44:10 +0530

    Time: 13 ms (51.02%), Space: 42.6 MB (66.62%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.java
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -1,22 +1,45 @@
 class Solution {
-    public static String decodeMessage(String key, String message) {
-        int len = key.length();
+    // public static String decodeMessage(String key, String message) {
+    //     int len = key.length();
+    //     String res="";
+    //     HashMap<Character, Character> mp = new HashMap<>();
+    //     char start='a';
+    //     for (int i=0;i<len;i++) {
+    //         if(!mp.containsKey(key.charAt(i)) && key.charAt(i) !=' ') {
+    //             mp.put(key.charAt(i),start++);
+    //         }
+    //     }
+    //     for(int i=0;i<message.length();i++) {
+    //         if(message.charAt(i)==' '){
+    //             res+=' ';
+    //         }
+    //         else if(mp.containsKey(message.charAt(i))) {
+    //             res+=mp.get(message.charAt(i));
+    //         }
+    //     }
+    //     return res;
+    // }
+        public static String decodeMessage(String key, String message) {
+        int a[]=new int[128];
+        Arrays.fill(a,-1);
+        char start=97;
+        // System.out.println('a'-32);
+        for(int i=0;i<key.length();i++){
+           int idx=key.charAt(i)-32;
+//            System.out.println(idx);
+            if(a[idx]==-1 && key.charAt(i)!=' '){
+                a[idx]=start++;
+            }
+        }
         String res="";
-        HashMap<Character, Character> mp = new HashMap<>();
-        char start='a';
-        for (int i=0;i<len;i++) {
-            if(!mp.containsKey(key.charAt(i)) && key.charAt(i) !=' ') {
-                mp.put(key.charAt(i),start++);
-            }
-        }
-        for(int i=0;i<message.length();i++) {
-            if(message.charAt(i)==' '){
+        for(int i=0;i<message.length();i++){
+            if(message.charAt(i) ==' '){
                 res+=' ';
-            }
-            else if(mp.containsKey(message.charAt(i))) {
-                res+=mp.get(message.charAt(i));
+            }else{
+                res+=(char)a[message.charAt(i)-32];
             }
         }
+        // System.out.println(Arrays.toString(a));
         return res;
     }
 }
