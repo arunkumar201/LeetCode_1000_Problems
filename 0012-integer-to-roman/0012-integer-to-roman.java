c709f4ef5a778f8dd80835ad20306a968c191839
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-15 10:29:54 +0530

    Time: 16 ms (26.22%), Space: 43.1 MB (54.50%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,87 +1,20 @@
 class Solution {
-   public String intToRoman(int num) {
+    public String intToRoman(int n) {
+     
+    String units[]={"","I","II","III","IV","V","VI","VII","VIII","IX"};
         
-                StringBuilder builder = new StringBuilder();
-                int numCopy = num;
-                int iterations;
-                int cipher = 1000;
+ String tens[]={"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        
+String hundreds[]=  {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        
+String thousands[]={"","M","MM","MMM"};
 
-                if(numCopy >= 1000) {
-                    iterations = numCopy/1000;
-                    while (iterations > 0) {
-                        builder.append('M');
-                        iterations--;
-                    }
-                    numCopy = numCopy % 1000;
-                }
-
-                if(numCopy >= 900) {
-                    builder.append('C');
-                    builder.append('M');
-                    numCopy = numCopy % 900;
-                }
-
-                if(numCopy >= 500) {
-                    builder.append('D');
-                    numCopy = numCopy % 500;
-                }
-                if(numCopy >= 400) {
-                    builder.append('C');
-                    builder.append('D');
-                    numCopy = numCopy % 400;
-                }
-                if(numCopy >= 100) {
-                    iterations = numCopy/100;
-                    while (iterations > 0) {
-                        builder.append('C');
-                        iterations--;
-                    }
-                    numCopy = numCopy % 100;
-                }
-                if(numCopy >= 90) {
-                    builder.append('X');
-                    builder.append('C');
-                    numCopy = numCopy % 90;
-                }
-                if(numCopy >= 50) {
-                    builder.append('L');
-                    numCopy = numCopy % 50;
-                }
-                if(numCopy >= 40) {
-                    builder.append('X');
-                    builder.append('L');
-                    numCopy = numCopy % 40;
-                }
-                if(numCopy >= 10) {
-                    iterations = numCopy/10;
-                    while (iterations > 0) {
-                        builder.append('X');
-                        iterations--;
-                    }
-                    numCopy = numCopy % 10;
-                }
-                if(numCopy >= 9) {
-                    builder.append('I');
-                    builder.append('X');
-                    numCopy = numCopy % 9;
-                }
-                if(numCopy >= 5) {
-                    builder.append('V');
-                    numCopy = numCopy % 5;
-                }
-                if(numCopy >= 4) {
-                    builder.append('I');
-                    builder.append('V');
-                    numCopy = numCopy % 4;
-                }
-                if(numCopy >= 1) {
-
-                    iterations = numCopy;
-                    while (iterations > 0) {
-                        builder.append('I');
-                        iterations--;
-                    }
-                }
-                return builder.toString();
+        
+        return (thousands[n/1000]+
+               hundreds[(n % 1000)/100]+
+                tens[(n%100)/10]+
+               units[n%10]);
+        
+        
     }
 }
