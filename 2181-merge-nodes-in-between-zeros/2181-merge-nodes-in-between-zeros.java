df17570d272d23dcf5d2d01562af23e83dfc5869
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-16 11:34:20 +0530

    Time: 7 ms (85.42%), Space: 80.1 MB (80.94%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.java
@@ -9,40 +9,27 @@
  * }
  */
 class Solution {
-   //   public ListNode mergeNodes(ListNode head) {
-   //      int sum=0;
-   //  ListNode dummy=new ListNode(-1);
-   //      ListNode l1=head;
-   //      ListNode l2=head;
-   //      ListNode d=dummy;
-   //      l2=l2.next;
-   // while(l2.next!=null){ 
-   //     if(l1.val==0 && l2.val!=0){
-   //         sum+=l2.val;
-   //         l2=l2.next;
-   //     }else{
-   //         d.next=new ListNode(sum);
-   //         d=d.next;
-   //         l1=l2;
-   //         l2=l2.next;
-   //         sum=0;   
-   //     }  
-   // }
-   //      return d;   
-   //  }
-   public ListNode mergeNodes(ListNode head){
-ListNode temp = head, temp2 = head.next;
-     int sum=0;
-      while(temp2!=null){
-            if(temp2.val==0){ 
-                temp.next.val=sum; 
-                temp.next.next=temp2.next;  
-                temp=temp.next; 
-                sum=0; 
-           }
-            sum+=temp2.val; 
-            temp2=temp2.next;
-        }
-        return head.next; 
+     public ListNode mergeNodes(ListNode head) {
+        int sum=0;
+    ListNode dummy=new ListNode(-1);
+        ListNode l1=head;
+        ListNode l2=head;
+        ListNode d=dummy;
+        l2=l2.next;
+   while(l2!=null){ 
+       if(l1.val==0 && l2.val!=0){
+           sum+=l2.val;
+           l2=l2.next;
+       }else{
+           ListNode nd=new ListNode(sum);
+           d.next=nd;
+           d=nd;
+           l1=l2;
+           l2=l2.next;
+           sum=0;   
+       }  
+   }
+        return dummy.next;   
     }
+ 
 }
