629265e2bb2d7d84bc0866a10905cbb7236e791b
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-17 22:00:30 +0530

    Time: 1 ms (100.00%), Space: 39.4 MB (79.11%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isPowerOfFour(int n) {      
-    while(n>1){
-        if(n%4!=0) return false;
-        n/=4;
-    }
-        return n==1;
+  int a[]={1,4,16,64,256,1024,4096,16384,65536,262144,1048576,4194304,16777216,67108864,268435456,1073741824};
+        for(int i:a){
+            if(n==i) return true;
+        }
+        return false;
     }
 }
