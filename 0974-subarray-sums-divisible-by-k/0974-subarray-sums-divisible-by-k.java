31112d3696d35e71d04ef92d1e46cd20315f6d8a
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-19 11:19:00 +0530

    Time: 26 ms (44.22%), Space: 46.9 MB (69.94%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -2,16 +2,7 @@ class Solution {
  public static int subarraysDivByK(int[] a, int k) {
           int curr_sum=0;
           int count=0;
-          int preSum[]=new int[a.length];
-          int x = 0;
-          int Running_Sum=0;
-          for(int j: a){
-              Running_Sum+=j;
-              preSum[x++]=Running_Sum;
-          }
-        // System.out.println(Arrays.toString(preSum));
         HashMap<Integer,Integer> mp=new HashMap<Integer,Integer>();
-
         for(int i=0;i<a.length;i++){
             curr_sum+=a[i];
             int rem=curr_sum%k;
