bf2fcde91b0b4cd485dcdf28baf3c3a58085aa14
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-25 22:11:39 +0530

    Time: 64 ms (21.62%), Space: 45.5 MB (40.83%) - LeetHub

--- 0475-heaters/0475-heaters.java
+++ 0475-heaters/0475-heaters.java
@@ -1,41 +1,16 @@
 class Solution {
-     static int[] pair =new int[]{-1,-1};
-
-   static int[] Binary_Search(int t,int[] a){
-        int l=0;
-        int h=a.length-1;
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(a[mid]==t){
-                pair[0]=a[mid];
-                pair[1]=a[mid];
-                break;
-            }
-            if(a[mid]<t){
-               pair[0]=a[mid];
-                l=mid + 1;
-            }else{
-                pair[1]=a[mid];
-                  h=mid -1;
-            }
-        }
-        return pair;
-    }
-
-    public static int findRadius(int[] houses, int[] heaters){
-        Arrays.sort(heaters);
-        int radius =Integer.MIN_VALUE;
-        for(int i=0;i<houses.length; i++){
-            int housePos=houses[i];
-            pair= Binary_Search(housePos,heaters);
-            int dis1=(pair[0]==-1) ? Integer.MAX_VALUE : housePos-pair[0];
-            int dis2=(pair[1]==-1) ? Integer.MAX_VALUE : pair[1]-housePos;
-            int mid=Math.min(dis1,dis2);
-            radius=Math.max(radius,mid);
-            pair[0]=-1;
-            pair[1]=-1;
-         
-        }
-       return radius;
+  public int findRadius(int[] houses, int[] heaters) {
+        TreeSet<Integer> set = new TreeSet<>(); 
+        for (int heater : heaters)  
+            set.add(heater); 
+        
+        int index = 0, res = 0; 
+        for (int house : houses) {
+            int dist1 = set.ceiling(house) == null ? Integer.MAX_VALUE : set.ceiling(house) - house; 
+            int dist2 = set.floor(house) == null ? Integer.MAX_VALUE : house - set.floor(house); 
+            res = Math.max(res, Math.min(dist1, dist2)); 
+        }    
+        
+        return res; 
     }
 }
