a79524136c8f36121bf85c1e1471dea526915ce7
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-25 22:07:23 +0530

    Time: 73 ms (17.03%), Space: 53.9 MB (28.60%) - LeetHub

--- 0475-heaters/0475-heaters.java
+++ 0475-heaters/0475-heaters.java
@@ -1,17 +1,41 @@
 class Solution {
-public static int findRadius(int[] houses, int[] heaters) {
-          // Arrays.sort(houses);
-          // Arrays.sort(heaters);
-          int radius =Integer.MIN_VALUE;
-          int diff=Integer.MAX_VALUE;
-          for(int k=0;k<houses.length;k++) {
-               for(int l=0;l< heaters.length;l++) {
-                   int  x=Math.abs(houses[k]-heaters[l]);
-                   diff=Math.min(diff,x);
-               }
-              radius = Math.max(diff,radius);
-               diff=Integer.MAX_VALUE;
-          }
-          return radius;
+     static int[] pair =new int[]{-1,-1};
+
+   static int[] Binary_Search(int t,int[] a){
+        int l=0;
+        int h=a.length-1;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(a[mid]==t){
+                pair[0]=a[mid];
+                pair[1]=a[mid];
+                break;
+            }
+            if(a[mid]<t){
+               pair[0]=a[mid];
+                l=mid + 1;
+            }else{
+                pair[1]=a[mid];
+                  h=mid -1;
+            }
+        }
+        return pair;
+    }
+
+    public static int findRadius(int[] houses, int[] heaters){
+        Arrays.sort(heaters);
+        int radius =Integer.MIN_VALUE;
+        for(int i=0;i<houses.length; i++){
+            int housePos=houses[i];
+            pair= Binary_Search(housePos,heaters);
+            int dis1=(pair[0]==-1) ? Integer.MAX_VALUE : housePos-pair[0];
+            int dis2=(pair[1]==-1) ? Integer.MAX_VALUE : pair[1]-housePos;
+            int mid=Math.min(dis1,dis2);
+            radius=Math.max(radius,mid);
+            pair[0]=-1;
+            pair[1]=-1;
+         
+        }
+       return radius;
     }
 }
