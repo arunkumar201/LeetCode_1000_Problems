d0ff92e99c6055343cf566b109876c6af7b2da9f
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-26 22:09:57 +0530

    Time: 33 ms (13.17%), Space: 51.2 MB (14.74%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int change(int sum, int[] coins) {
+              // for storing solutions to subproblems:
+    	int size = coins.length;
+    	int[][] arr = new int[size + 1][sum + 1];
+    	
+      // Initializing first column of array to 1
+      // because a sum of 0 can be made
+      // in one possible way: {0}
+    	for(int i = 0; i < size + 1; i++)
+    		arr[i][0] = 1;
+    	
+      // Applying the recursive solution:
+    	for(int i = 1; i < size + 1; i++)
+    		for(int j = 0; j < sum + 1; j++)
+    			if(coins[i - 1] > j)  // Cannot pick the highest coin:
+    				arr[i][j] = arr[i - 1][j];
+    			else  // Pick the highest coin:
+    				arr[i][j] = arr[i][j - coins[i - 1]] + arr[i - 1][j];
+    	
+    	return arr[size][sum];
+    
+    }
+}
