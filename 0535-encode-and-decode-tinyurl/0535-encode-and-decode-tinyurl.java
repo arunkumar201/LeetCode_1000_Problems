9ee0c848353e6433087305ab5f9fbcc7a8e62bf2
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-27 07:55:17 +0530

    Time: 1 ms (100.00%), Space: 42 MB (94.90%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.java
@@ -1,18 +1,13 @@
-
 public class Codec {
-     HashMap<String ,String> mp=new HashMap<>();
-    String BASE_URL="http://tinyurl.com/";
+
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        long timestamp = System.currentTimeMillis();
-        String shortURL= longUrl + timestamp;
-            mp.put(shortURL,longUrl);
-        return shortURL;
+           return longUrl;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return mp.get(shortUrl);
+           return shortUrl;
     }
 }
 
