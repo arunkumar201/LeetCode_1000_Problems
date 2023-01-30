3b0cd7d0e50e608ca7044b0d95539df5748393a0
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2023-01-30 07:32:47 +0530

    Time: 2 ms (73.06%), Space: 40.3 MB (92.60%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,13 +1,13 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-       char[] sChars = s.toCharArray();
-    char[] tChars = t.toCharArray();
-    Arrays.sort(sChars);
-    Arrays.sort(tChars);
-    for (int i = 0; i < t.length(); i++) {
-        if (i < s.length() && sChars[i] != tChars[i]) return tChars[i];
-        if (i == s.length()) return tChars[i];
-    }
+       int[] ch =new int[26];
+        for(int i = 0; i < t.length(); i++){
+           ch[t.charAt(i) - 'a']++;
+            if(i<s.length())  ch[s.charAt(i) - 'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(ch[i]==1) return (char) (i + 'a');
+        }
     return ' ';
 }
 
