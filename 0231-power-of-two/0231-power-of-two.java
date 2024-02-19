d83a7fdc944deda1b7d07d01d1b94b5ede30982f
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2024-02-19 12:55:49 +0530

    Time: 1 ms (49.23%), Space: 41.1 MB (14.30%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -3,10 +3,6 @@ class Solution {
         if (n <= 0) {
             return false;
         }
-        // double logResult = Math.log(n) / Math.log(2);
-        // return Math.abs(logResult - Math.round(logResult)) < 1e-10;
-                return ((n & (n - 1)) == 0);
-
-    }
-
+       return (n & (n-1))==0;
+}
 }
