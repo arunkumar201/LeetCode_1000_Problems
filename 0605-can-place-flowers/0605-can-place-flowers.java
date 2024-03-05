5cd46c6a238f122d1feb6617f0957e4fa98ca12c
ARUN KUMAR <70876302+arunkumar201@users.noreply.github.com>
2024-03-05 06:23:03 +0530

    Time: 1 ms (96.54%), Space: 45.2 MB (89.64%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -1,17 +1,30 @@
 class Solution {
-   public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int size = flowerbed.length;
-        int[] modifyFlower = new int[size + 2];
-        for (int j = 1; j < modifyFlower.length - 1; j++) {
-            modifyFlower[j] = flowerbed[j - 1];
-        }
+//    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+//         int size = flowerbed.length;
+//         int[] modifyFlower = new int[size + 2];
+//         for (int j = 1; j < modifyFlower.length - 1; j++) {
+//             modifyFlower[j] = flowerbed[j - 1];
+//         }
 
-        for (int i = 1; i < modifyFlower.length-1; i++) {
-            if (modifyFlower[i] == 0 && modifyFlower[i + 1] == 0 && modifyFlower[i - 1] == 0) {
-                n--;
-                modifyFlower[i] = 1;
-            }
+//         for (int i = 1; i < modifyFlower.length-1; i++) {
+//             if (modifyFlower[i] == 0 && modifyFlower[i + 1] == 0 && modifyFlower[i - 1] == 0) {
+//                 n--;
+//                 modifyFlower[i] = 1;
+//             }
+//         }
+//         return n <= 0;
+//     }
+       public  boolean canPlaceFlowers(int[] flowerbed, int n) {
+
+        for(int i=0;i<flowerbed.length;i++){
+           int prev=i==0?0:flowerbed[i-1];
+           int next=i==flowerbed.length-1?0:flowerbed[i+1];
+           if(flowerbed[i]==0 && prev==0 && next==0){
+               flowerbed[i]=1;
+               n--;
+           }
         }
-        return n <= 0;
+        return n<=0;
+
     }
 }
